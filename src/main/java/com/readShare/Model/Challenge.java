59ee5cf2bd6e7435cb724347f7465ce97e8e4a92
barış tiftik <brstftk@hotmail.com>
2021-05-10 21:17:13 +0300

    Challenge.java getters and setters

--- src/main/java/com/readShare/Model/Challenge.java
+++ src/main/java/com/readShare/Model/Challenge.java
@@ -8,4 +8,45 @@ public class Challenge {
     private int duration;
     private User winner;
     private int participantNumber;
+
+    // get and set methods
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id = id;
+    }
+
+    public Date getChallengeDate() {
+        return challengeDate;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public User getWinner() {
+        return winner;
+    }
+
+    public int getParticipantNumber() {
+        return participantNumber;
+    }
+
+    public void setChallengeDate(Date challengeDate) {
+        this.challengeDate = challengeDate;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public void setWinner(User winner) {
+        this.winner = winner;
+    }
+
+    public void setParticipantNumber(int participantNumber) {
+        this.participantNumber = participantNumber;
+    }
 }
